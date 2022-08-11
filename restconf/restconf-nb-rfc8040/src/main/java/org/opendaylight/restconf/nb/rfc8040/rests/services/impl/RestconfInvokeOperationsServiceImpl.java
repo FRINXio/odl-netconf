opendaylight/netconf

Netconf Device Notification
Requirement - HTTP client should be able to get Device notification
after subscription.

Implementation-

1. Added a new Device Notification Yang for Rest request which takes
 input of Device mount point path
2. Added a DeviceNotificationListenerAdaptor which is get registered
in Device Notification service (on Mount point)
3. Created a dynamic Rest Notification SSE HTTP rest url (Response of 1st rest
call )
4. Updated Test tool for Notification

JIRA: NETCONF-745
Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>
Change-Id: I9cac35d0a7b0f79382d223ae4ec0ac4a3ce2bae9
Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>

restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfInvokeOperationsServiceImpl.java
@@ -24,6 +24,7 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
 import org.opendaylight.mdsal.dom.api.DOMRpcException;
 import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
@@ -33,6 +34,7 @@ import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
 import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 import org.opendaylight.restconf.nb.rfc8040.rests.services.api.RestconfInvokeOperationsService;
+import org.opendaylight.restconf.nb.rfc8040.streams.Configuration;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.opendaylight.yangtools.yang.common.QName;
@@ -58,14 +60,23 @@ public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperat
     // FIXME: at some point we do not want to have this here
     private static final XMLNamespace SAL_REMOTE_NAMESPACE =
         XMLNamespace.of("urn:opendaylight:params:xml:ns:yang:controller:md:sal:remote");
+    private static final XMLNamespace DEVICE_NOTIFICATION_NAMESPACE =
+            XMLNamespace.of("device:notification");
 
     private final DOMRpcService rpcService;
     private final SchemaContextHandler schemaContextHandler;
+    private final DOMMountPointService mountPointService;
+    private final SubscribeToStreamUtil streamUtils;
 
     public RestconfInvokeOperationsServiceImpl(final DOMRpcService rpcService,
-            final SchemaContextHandler schemaContextHandler) {
+                                               final SchemaContextHandler schemaContextHandler,
+                                               final DOMMountPointService mountPointService,
+                                               final Configuration configuration) {
         this.rpcService = requireNonNull(rpcService);
         this.schemaContextHandler = requireNonNull(schemaContextHandler);
+        this.mountPointService = requireNonNull(mountPointService);
+        this.streamUtils  = configuration.isUseSSE() ? SubscribeToStreamUtil.serverSentEvents()
+                : SubscribeToStreamUtil.webSockets();
     }
 
     @Override
@@ -76,6 +87,7 @@ public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperat
         final DOMMountPoint mountPoint = context.getMountPoint();
         final SchemaNode schema = context.getSchemaNode();
         final QName rpcName = schema.getQName();
+        boolean isDeviceNotification = DEVICE_NOTIFICATION_NAMESPACE.equals(rpcName.getNamespace());
 
         final ListenableFuture<? extends DOMRpcResult> future;
         if (mountPoint == null) {
@@ -88,6 +100,10 @@ public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperat
                     future = Futures.immediateFailedFuture(new RestconfDocumentedException("Unsupported operation",
                         ErrorType.RPC, ErrorTag.OPERATION_NOT_SUPPORTED));
                 }
+            } else if (isDeviceNotification) {
+                future = Futures.immediateFuture(
+                        CreateStreamUtil.createDeviceNotificationListener(uriInfo, payload, schemaContext,
+                                streamUtils, mountPointService));
             } else {
                 future = invokeRpc(payload.getData(), rpcName, rpcService);
             }

FRINXio/odl-netconf

4. Updated Test-tool for Notification
(cherry picked from commit 6e535fd00d087ef12c668eadf8befd4e3ddc1c5e)

restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfInvokeOperationsServiceImpl.java
@@ -25,6 +25,7 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
 import org.opendaylight.mdsal.dom.api.DOMRpcException;
 import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
@@ -35,6 +36,7 @@ import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
 import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 import org.opendaylight.restconf.nb.rfc8040.rests.services.api.RestconfInvokeOperationsService;
+import org.opendaylight.restconf.nb.rfc8040.streams.Configuration;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.opendaylight.yangtools.yang.common.QName;
@@ -62,14 +64,23 @@ public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperat
     // FIXME: at some point we do not want to have this here
     private static final XMLNamespace SAL_REMOTE_NAMESPACE =
         XMLNamespace.of("urn:opendaylight:params:xml:ns:yang:controller:md:sal:remote");
+    private static final XMLNamespace DEVICE_NOTIFICATION_NAMESPACE =
+            XMLNamespace.of("device:notification");
 
     private final DOMRpcService rpcService;
     private final SchemaContextHandler schemaContextHandler;
+    private final DOMMountPointService mountPointService;
+    private final SubscribeToStreamUtil streamUtils;
 
     public RestconfInvokeOperationsServiceImpl(final DOMRpcService rpcService,
-            final SchemaContextHandler schemaContextHandler) {
+                                               final SchemaContextHandler schemaContextHandler,
+                                               final DOMMountPointService mountPointService,
+                                               final Configuration configuration) {
         this.rpcService = requireNonNull(rpcService);
         this.schemaContextHandler = requireNonNull(schemaContextHandler);
+        this.mountPointService = requireNonNull(mountPointService);
+        this.streamUtils  = configuration.isUseSSE() ? SubscribeToStreamUtil.serverSentEvents()
+                : SubscribeToStreamUtil.webSockets();
     }
 
     @Override
@@ -78,6 +89,7 @@ public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperat
         final SchemaNode schema = payload.getInstanceIdentifierContext().getSchemaNode();
         final QName rpcName = schema.getQName();
         final DOMMountPoint mountPoint = payload.getInstanceIdentifierContext().getMountPoint();
+        boolean isDeviceNotification = DEVICE_NOTIFICATION_NAMESPACE.equals(rpcName.getNamespace());
 
         final ListenableFuture<? extends DOMRpcResult> future;
         final EffectiveModelContext schemaContextRef;
@@ -93,6 +105,10 @@ public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperat
                     future = Futures.immediateFailedFuture(new RestconfDocumentedException("Unsupported operation",
                         ErrorType.RPC, ErrorTag.OPERATION_NOT_SUPPORTED));
                 }
+            } else if (isDeviceNotification) {
+                future = Futures.immediateFuture(
+                        CreateStreamUtil.createDeviceNotificationListener(uriInfo, payload, schemaContextRef,
+                                streamUtils, mountPointService));
             } else {
                 future = invokeRpc(payload.getData(), rpcName, rpcService);
             }
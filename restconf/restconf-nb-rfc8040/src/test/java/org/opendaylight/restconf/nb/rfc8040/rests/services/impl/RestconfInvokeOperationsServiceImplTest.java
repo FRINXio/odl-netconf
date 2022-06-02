e372c0169737c6ce1c46e1b26a4b546789111c7e
Nikhil Soni <nikk.sonitech@gmail.com>
2022-06-02 19:05:48 +0200

    Netconf Device Notification
    Requirement - HTTP client should be able to get Device notification
    after subscription.
    
    Implementation-
    
    1. Added a new Device Notification Yang for Rest request which takes
     input of Device mount point path
    2. Added a DeviceNotificationListenerAdaptor which is get registered
    in Device Notification service (on Mount point)
    3. Created a dynamic Rest Notification SSE HTTP rest url (Response of 1st rest
    call )
    4. Updated Test-tool for Notification
    
    JIRA: NETCONF-745
    Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>
    Change-Id: I9cac35d0a7b0f79382d223ae4ec0ac4a3ce2bae9
    Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>
    Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>
    (cherry picked from commit 6e535fd00d087ef12c668eadf8befd4e3ddc1c5e)

--- restconf/restconf-nb-rfc8040/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfInvokeOperationsServiceImplTest.java
+++ restconf/restconf-nb-rfc8040/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfInvokeOperationsServiceImplTest.java
@@ -38,6 +38,7 @@ import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
 import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
 import org.opendaylight.mdsal.dom.api.DOMRpcException;
 import org.opendaylight.mdsal.dom.api.DOMRpcImplementationNotAvailableException;
 import org.opendaylight.mdsal.dom.api.DOMRpcResult;
@@ -49,6 +50,7 @@ import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.nb.rfc8040.TestRestconfUtils;
 import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
 import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
+import org.opendaylight.restconf.nb.rfc8040.streams.Configuration;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.RpcError;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
@@ -78,6 +80,8 @@ public class RestconfInvokeOperationsServiceImplTest {
     private DOMRpcService rpcService;
     @Mock
     private DOMMountPoint mountPoint;
+    @Mock
+    private DOMMountPointService mountPointService;
     private RestconfInvokeOperationsServiceImpl invokeOperationsService;
 
     @Before
@@ -92,7 +96,8 @@ public class RestconfInvokeOperationsServiceImplTest {
             mock(DOMSchemaService.class));
         schemaContextHandler.onModelContextUpdated(contextRef);
         invokeOperationsService =
-                new RestconfInvokeOperationsServiceImpl(rpcService, schemaContextHandler);
+                new RestconfInvokeOperationsServiceImpl(rpcService, schemaContextHandler, mountPointService,
+                        mock(Configuration.class));
     }
 
     @Test
